9475d758e05d43cb674d0bcdcd155f679a5aba72
kuroshi_dev <kuroshit23@gmail.com>
2025-06-12 01:06:44 +0300

    Fixed Encoding

--- lab6/main.py
+++ lab6/main.py
@@ -7,6 +7,8 @@ def analyze_news_page(url):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
     response = requests.get(url, headers=headers) # 1. Get Page Content
+    response.encoding = response.apparent_encoding
+    
     soup = BeautifulSoup(response.text, 'html.parser')
     
     text = soup.get_text() # 2. Word Count
